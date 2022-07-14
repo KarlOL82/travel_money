a089483bec6ddaf678904f59860fe93564d85937
Benjamin <bstanf@uw.edu>
2022-07-14 15:52:44 -0700

    testing out the fetch for the attractions

--- assets/js/opentripapi.js
+++ assets/js/opentripapi.js
@@ -1,15 +1,38 @@
 // Static api url to test for 10 attractions in a 3000m radius around Seattle.
 var requestUrl = 'https://api.opentripmap.com/0.1/en/places/radius?radius=3000&lon=-122.33207&lat=47.60621&kinds=baths_and_saunas,bridges&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a';
-var apiBaseUrl = 'https://api.opentripmap.com/0.1/en/places/radius'
+var apiBaseUrl = 'https://api.opentripmap.com/0.1/en/places/radius?radius=16000'
+var apiGeoLocUrl = 'https://api.opentripmap.com/0.1/en/places'
 
+var cityLat = "";
+var cityLon = "";
 
 // Basic fetch function to verify we are accessing the json data and console logging.
-function getApi() {
-    fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      });
+function getOpenTripApi(citySearch) {
+
+var getCityUrl = apiGeoLocUrl + "/geoname";
+  getCityUrl += "?name=" + citySearch + "&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a";
+  console.log(getCityUrl);
+
+return fetch(getCityUrl)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function (data){
+    console.log(data.lat);
+    console.log(data.lon);
+var getDestinations = apiBaseUrl+'&lon=${data.lon}&lat=${data.lat}&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a';
+   });
 }
+
+// function getDestinations(cityLon, cityLat) {
+// var getDestinations = apiBaseUrl+'&lon=${data.lon}&lat=${data.lat}&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a';
+// console.log(getDestinations);
+
+// // return fetch(getDestinations)
+// // .then(function(response){
+// //  return response.json();
+// // })
+// // .then(function (data){
+// //   console.log(data);
+// // })
+// }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var searchBtnEl = document.querySelector("#search-btn");
 var citySearchEl = document.querySelector("#city-search");
-
+var citySearch = "";
 
 
 
@@ -8,4 +8,5 @@ searchBtnEl.addEventListener("click", function(event) {
     console.log("Clicked");
     var citySearch = citySearchEl.value;
     console.log(citySearch);
+    getOpenTripApi(citySearch);
   });
