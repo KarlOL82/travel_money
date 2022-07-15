88db13449c38a4bf687b4bba67a7ad99b35ecf4b
Benjamin <bstanf@uw.edu>
2022-07-14 19:42:28 -0700

    OpenTrip Api functionality

--- assets/js/opentripapi.js
+++ assets/js/opentripapi.js
@@ -1,15 +1,16 @@
 // Static api url to test for 10 attractions in a 3000m radius around Seattle.
 var requestUrl = 'https://api.opentripmap.com/0.1/en/places/radius?radius=3000&lon=-122.33207&lat=47.60621&kinds=baths_and_saunas,bridges&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a';
-var apiBaseUrl = 'https://api.opentripmap.com/0.1/en/places/radius?radius=16000'
-var apiGeoLocUrl = 'https://api.opentripmap.com/0.1/en/places'
+var apiBaseUrl = 'https://api.opentripmap.com/0.1/en/places'
 
-var cityLat = "";
-var cityLon = "";
+var cityLon;
+var cityLat;
+var attractionArray;
+var uniqueId;
 
-// Basic fetch function to verify we are accessing the json data and console logging.
+// Main fetch function to geolocate
 function getOpenTripApi(citySearch) {
 
-var getCityUrl = apiGeoLocUrl + "/geoname";
+var getCityUrl = apiBaseUrl + "/geoname";
   getCityUrl += "?name=" + citySearch + "&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a";
   console.log(getCityUrl);
 
@@ -20,19 +21,46 @@ return fetch(getCityUrl)
   .then(function (data){
     console.log(data.lat);
     console.log(data.lon);
-var getDestinations = apiBaseUrl+'&lon=${data.lon}&lat=${data.lat}&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a';
+   cityLon = data.lon;
+   cityLat = data.lat;
+   getDestinations();
    });
 }
 
-// function getDestinations(cityLon, cityLat) {
-// var getDestinations = apiBaseUrl+'&lon=${data.lon}&lat=${data.lat}&format=json&limit=10&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a';
-// console.log(getDestinations);
+// Fetches the attractions within a 10 miles from the geolocation
+function getDestinations(){
+  var attractionsUrl = apiBaseUrl+"/radius?radius=16000&lon="+cityLon+"&lat="+cityLat+"&format=json&limit=5&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a";
+console.log(attractionsUrl);
+  return fetch(attractionsUrl)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+    
+    for(var i=0; i < data.length; i++) {
+  // var  detailsUrl = apiBaseUrl+"/xid/"+data[i].xid+"&apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a";
+  // console.log(data[i].xid);
+  uniqueId = data[i].xid;
+  getDetails();
 
-// // return fetch(getDestinations)
-// // .then(function(response){
-// //  return response.json();
-// // })
-// // .then(function (data){
-// //   console.log(data);
-// // })
+}
+  });
+
+}
+
+function getDetails(){
+  
+// for(var i=0; i < data.length; i++) {
+  var  detailsUrl = apiBaseUrl+"/xid/"+uniqueId+"?apikey=5ae2e3f221c38a28845f05b6b20dacbb21b5be1ba73523de4298d37a";
+  // console.log(detailsUrl);
 // }
+
+  return fetch(detailsUrl)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+  })
+}
