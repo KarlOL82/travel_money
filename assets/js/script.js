6e73ebe066cbef996517048448add3a86975c07a
adhillon0817 <avleendhillon@gmail.com>
2022-07-17 14:41:39 -0700

    favorites buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,7 @@ function displayAttractions(data) {
               ${data.wikipedia_extracts.text}
             </p>
           </div>
-         <button>Button</button>     
+          <button class="saveButton">Add to Favorites!</button>    
         </div>
       </article>
     </div>`;
